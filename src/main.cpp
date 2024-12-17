9cdd6d72e02f659b9b5fc45ec6ea9538af91fd97
Rafael Santos <rbts123456789@gmail.com>
2024-12-17 22:35:04 +0000

    ESP camera dependency. RFID configuration test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
 
-void setup() {
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+String tagId = "None";
+byte nuidPICC[4];
+
+void setup(void)
+{
+    Serial.begin(115200);
+    Serial.println("System initialized");
+    nfc.begin();
 }
 
-void loop() {
+void loop()
+{
+    if (nfc.tagPresent())
+    {
+        NfcTag tag = nfc.read();
+        tag.print();
+        tagId = tag.getUidString();
+        Serial.println(tagId);
+    }
+    delay(5000);
 }
