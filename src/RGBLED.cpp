b3d8b1672824cc66c2dce885827b419111691d1e
Feroconas <rafatinoni@gmail.com>
2024-12-24 17:05:38 +0000

    Pedro testa pls

--- src/RGBLED.cpp
+++ src/RGBLED.cpp
@@ -14,7 +14,6 @@ void RGBLED::setColor(Color color, unsigned long duration) {
 }
 
 void RGBLED::update() {
-    if (endTime == 0 || millis() < endTime)
-        return;
-    setColor(OFF);
+    if (endTime > 0 && millis() > endTime)
+        setColor(OFF);
 }
