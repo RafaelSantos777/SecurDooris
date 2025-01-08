d1c64ce63c389e54301c0f78ede822341fcc3eaa
Feroconas <rafatinoni@gmail.com>
2025-01-08 19:51:20 +0000

    Test camera with Serial instead of Serial1

--- test/CodeForCameraConnectionTest.cpp
+++ test/CodeForCameraConnectionTest.cpp
@@ -5,15 +5,15 @@ void setup() {
     pinMode(LED, OUTPUT);      // set LED pin as output
     digitalWrite(LED, LOW);    // switch off LED pin
 
-    Serial1.begin(115200);            // initialize UART with baud rate of 9600
+    Serial.begin(9600);            // initialize UART with baud rate of 9600
 }
 void loop() {
-    while (Serial1.available() >= 0) {
-        int receivedData = Serial1.read();   // read one byte from serial buffer and save to receivedData
-        if (receivedData == 1) {
+    while (Serial.available() >= 0) {
+        String receivedData = Serial.readString();   // read one byte from serial buffer and save to receivedData
+        if (receivedData == "amogus") {
             digitalWrite(LED, HIGH); // switch LED On
         }
-        else if (receivedData == 0) {
+        else if (receivedData == "sus") {
             digitalWrite(LED, LOW);  // switch LED Off
         }
     }
--- test/CodeForNanoConnectionTest.cpp
+++ test/CodeForNanoConnectionTest.cpp
@@ -4,17 +4,17 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
   digitalWrite(LED_BUILTIN, LOW);    // switch off LED pin
 
-  Serial.begin(115200);               // initialize serial communication at 9600 bits per second:
-  Serial1.begin(115200);            // initialize UART with baud rate of 9600
+  Serial.begin(9600);               // initialize serial communication at 9600 bits per second:
+  Serial1.begin(9600);            // initialize UART with baud rate of 9600
 }
 void loop() {
-  if (Serial.read() == '1') {
-    Serial1.write((byte)1);
+  if (Serial.read() == 1) {
+    Serial1.print("amogus");
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println("LEDS ON");
   }
-  else if (Serial.read() == '0') {
-    Serial1.write((byte)0);
+  else if (Serial.read() == 0) {
+    Serial1.print("sus");
     digitalWrite(LED_BUILTIN, LOW);
     Serial.println("LEDS OFF");
   }
